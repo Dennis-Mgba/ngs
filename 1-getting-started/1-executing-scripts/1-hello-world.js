d05837b7d72d73ec91afaf0c3189a835a12616ac
Dennis Mgbah <dennismgbah@gmail.com>
2020-10-18 17:23:39 +0100

    getting started, executings scripts in node

--- 1-getting-started/1-executing-scripts/1-hello-world.js
+++ 1-getting-started/1-executing-scripts/1-hello-world.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(4242, () => {
+server.listen(5000, '127.0.0.1', () => {
   console.log('Server is running...');
 });
